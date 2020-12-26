e3090892a908cf5f69ce2df6d06b4b8e0c0b117d
ThanushiWickramanayaka <73863629+ThanushiWickramanayaka@users.noreply.github.com>
2020-12-26 11:29:10 +0530

    Create contact-form-handler.php

---
+++ JavaScript/contact-form-handler.php
@@ -0,0 +1,14 @@
+$message=$_POST['message'];
+
+$email_form='thanushiwickramanayaka@gmail.com';
+$email_subject="New Form Submission";
+$email_body="User Name:$name.\n".
+              "User Message:$message.\n";
+              $to="pasinduwickramanayaka@gmail.com";
+
+              $headers="Form: $email_form\r\n";
+              $headers .="Reply-To:$visitor_email\r\n";
+              mail($to,$email_subject,$email_body,$headers);
+              header("Location:qwe.html");
+
+?>
